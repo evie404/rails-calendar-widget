a4f3e51b133a834f5c4485649016f96aca65d58e
Ricky Pai <rickyp999@gmail.com>
2013-04-13 16:41:55 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CalendarWidget::Application.config.secret_key_base = 'cee255230118605fdedcc7ebdf6d6303ed9a794a5df43c656ddd0ec7e367b1257f6714d091e55b49e11b51f3955eff8af4e1f0d1b755b7703384ac1c50a15fcc'
